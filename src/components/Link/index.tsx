db4621dd3f93ad99c0ebd91fa848453c9784e9e4
jaum1234 <joaovitorsouzacoura@gmail.com>
2023-02-21 13:16:45 -0300

    :recycle: refactor: converting Link component to .ts.

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,6 +1,13 @@
 import NextLink from 'next/link';
 
-const Link = ({ children, href, target, locale, ...restProps }) => {
+type LinkProps = {
+    children: JSX.Element,
+    href?: string | undefined | any,
+    target?: any,
+    locale?: string | false | undefined
+}
+
+const Link = ({ children, href, target, locale, ...restProps }: LinkProps) => {
     return (
         <NextLink href={ href } locale={ locale }> 
             <a { ...restProps } target={ target }>
